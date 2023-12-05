0e04de0de6367097340ca8dd7b3d4f56b0873dc2
vincent-kipngeno <kipngenovi2001@gmail.com>
2023-12-05 10:58:27 +0300

    Create basic jwt security project to check and get an update on the deprecated features

---
+++ src/main/java/com/vince/security/SecurityApplication.java
@@ -0,0 +1,16 @@
+package com.vince.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+import com.vince.security.user.User;
+
+@SpringBootApplication
+public class SecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityApplication.class, args);
+	}
+
+}
